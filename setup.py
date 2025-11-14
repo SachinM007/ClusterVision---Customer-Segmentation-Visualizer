af782cded40c9d8317aef17537a2a4f5ddf93063
sachinm007 <bsachinmiryala@gmail.com>
2025-11-14 09:50:10 -0700

    created project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="ClusterVision",
+    description="Cusomer Segmentation Visualizer",
+    author="Sachin Miryala",
+    version="0.0.1",
+    packages=find_packages()
+)
